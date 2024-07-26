a4d81d2be969e74db458a7d353247fd8f577e279
Raiele Leite <raiele.leite@hotmail.com>
2024-07-25 22:19:44 -0300

    finalizando

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <string.h>
+#include <time.h>
 
 GtkBuilder *builder;
 GtkWidget *window;
@@ -10,9 +11,12 @@ GtkWidget *entry_senha;
 GtkWidget *entry_nome_cad;
 GtkWidget *entry_login_cad;
 GtkWidget *entry_senha_cad;
+GtkWidget *horario_chegada;
+GtkWidget *horario_saida;
 GtkStack  *stack;
+GtkListStore *modelodearmazenamento;
 
-void save_to_file_login(const gchar *login_text, const gchar *senha_text) {
+void save_to_file_login(const gchar *login_text, const gchar *senha_text, const gchar *horario_text, const gchar *buffer) {
     FILE *pArquivo_horario = fopen("Horario_de_entrada.txt", "a");
     if (pArquivo_horario == NULL) {
         g_print("Erro ao abrir o arquivo.\n");
@@ -21,6 +25,8 @@ void save_to_file_login(const gchar *login_text, const gchar *senha_text) {
 
     fprintf(pArquivo_horario, "Login: %s\n", login_text);
     fprintf(pArquivo_horario, "Senha: %s\n", senha_text);
+    fprintf(pArquivo_horario, "%s: %s\n", horario_text, buffer);
+
     fprintf(pArquivo_horario, "\n");
 
     fclose(pArquivo_horario);
@@ -51,8 +57,21 @@ void escrever_login() {
     const gchar *login_text = gtk_entry_get_text(GTK_ENTRY(entry_login));
     const gchar *senha_text = gtk_entry_get_text(GTK_ENTRY(entry_senha));
 
+    gboolean chegada = gtk_toggle_button_get_active(GTK_TOGGLE_BUTTON(horario_chegada));
+    gboolean saida = gtk_toggle_button_get_active(GTK_TOGGLE_BUTTON(horario_saida));
+
+    time_t t;
+    struct tm *tm_info;
+    char buffer[26];
+    time(&t);
+
+    tm_info = localtime(&t);
+    strftime(buffer, 26, "%d-%m-%Y %H:%M:%S", tm_info);
+
+    const gchar *horario_text = chegada ? "Horário de chegada" : (saida ? "Horário de saída" : "Horário desconhecido");
+
     login(login_text, senha_text);
-    save_to_file_login(login_text, senha_text);
+    save_to_file_login(login_text, senha_text, horario_text, buffer);
 
     // Limpar campos após o login
     gtk_entry_set_text(GTK_ENTRY(entry_login), "");
@@ -105,7 +124,28 @@ void on_button_listar_voltar_clicked(GtkWidget *widget, gpointer data) {
 }
 
 void on_button_listar_clicked(GtkWidget *widget, gpointer data) {
-    // Implementar a funcionalidade para listar se necessário
+    FILE *pArquivo_usuarios = fopen("Usuarios_registrados.txt", "r");
+    if (pArquivo_usuarios == NULL) {
+        g_print("Erro ao abrir o arquivo de usuários.\n");
+        return;
+    }
+
+    GtkTreeIter iter;
+    gtk_list_store_clear(modelodearmazenamento);
+
+    char line[256];
+    while (fgets(line, sizeof(line), pArquivo_usuarios)) {
+        // Remove o caractere de nova linha no final da string, se presente
+        size_t len = strlen(line);
+        if (len > 0 && line[len-1] == '\n') {
+            line[len-1] = '\0';
+        }
+
+        gtk_list_store_append(modelodearmazenamento, &iter);
+        gtk_list_store_set(modelodearmazenamento, &iter, 0, line, -1);
+    }
+
+    fclose(pArquivo_usuarios);
 }
 
 int main(int argc, char *argv[]) {
@@ -130,15 +170,15 @@ int main(int argc, char *argv[]) {
     // Obtenção do objeto da janela principal e dos campos de entrada
     stack = GTK_STACK(gtk_builder_get_object(builder, "stack"));
     window = GTK_WIDGET(gtk_builder_get_object(builder, "main_window"));
-
+    modelodearmazenamento = GTK_LIST_STORE(gtk_builder_get_object(builder, "liststore1"));
 
     entry_login = GTK_WIDGET(gtk_builder_get_object(builder, "entry_login"));
     entry_senha = GTK_WIDGET(gtk_builder_get_object(builder, "entry_senha"));
     entry_nome_cad = GTK_WIDGET(gtk_builder_get_object(builder, "entry_nome_cad"));
     entry_login_cad = GTK_WIDGET(gtk_builder_get_object(builder, "entry_login_cad"));
     entry_senha_cad = GTK_WIDGET(gtk_builder_get_object(builder, "entry_senha_cad"));
-
-
+    horario_chegada = GTK_WIDGET(gtk_builder_get_object(builder, "horario_chegada"));
+    horario_saida = GTK_WIDGET(gtk_builder_get_object(builder, "horario_saida"));
 
     gtk_widget_show_all(window);
     gtk_main();
